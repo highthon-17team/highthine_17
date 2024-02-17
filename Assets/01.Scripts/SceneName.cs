cdd84a9a269160298eee0f9c2287606f86d0f8b2
Ojunsun <98933734+Ojunsun@users.noreply.github.com>
2024-02-17 20:05:49 +0900

    ESC 설정

--- Assets/01.Scripts/SceneName.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SceneName : MonoBehaviour
-{
-    public const string Intro = "";
-    public const string InGame = "InGame";
-}
