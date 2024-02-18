907adf35542b266e84e78d92ad78e58357c44ea1
Taeng <xodn0720@naver.com>
2024-02-18 11:14:59 +0900

    flower

--- Assets/01.Scripts/Flower.cs
+++ Assets/01.Scripts/Flower.cs
@@ -11,6 +11,8 @@ public class Flower : MonoBehaviour
     public GameObject[] array_2;
     public GameObject[] array_3;
     public GameObject[] flowerArray; // 꽃 게임오브젝트 배열 추가
+    private float lastSpawnTime = 0f;
+    private float spawnInterval = 30f; // 꽃 생성 간격
 
     // Start is called before the first frame update
     void Start()
@@ -27,20 +29,24 @@ public class Flower : MonoBehaviour
         UnityEngine.Rendering.Universal.Light2D Sw_2 = Switch_2.GetComponent<UnityEngine.Rendering.Universal.Light2D>();
         UnityEngine.Rendering.Universal.Light2D Sw_3 = Switch_3.GetComponent<UnityEngine.Rendering.Universal.Light2D>();
 
-        if(Sw_1.intensity <= 0.7f)
+        // 각 스위치의 강도가 0.7 이하이고, 마지막 생성 시간에서 일정 시간이 경과한 경우에만 꽃 생성
+        if(Sw_1.intensity <= 0.7f && Time.time - lastSpawnTime >= spawnInterval)
         {
-            GameObject randomFlower_1 = flowerArray[Random.Range(0, flowerArray.Length)]; // 수정: 꽃 배열에서 랜덤한 값을 선택하도록 수정
+            GameObject randomFlower_1 = array_1[Random.Range(0, array_1.Length)]; 
             Instantiate(randomFlower_1, transform.position, Quaternion.identity);
+            lastSpawnTime = Time.time;
         }
-        if(Sw_2.intensity <= 0.7f)
+        if(Sw_2.intensity <= 0.7f && Time.time - lastSpawnTime >= spawnInterval)
         {
-            GameObject randomFlower_2 = flowerArray[Random.Range(0, flowerArray.Length)]; // 수정: 꽃 배열에서 랜덤한 값을 선택하도록 수정
+            GameObject randomFlower_2 = array_2[Random.Range(0, array_2.Length)]; 
             Instantiate(randomFlower_2, transform.position, Quaternion.identity);
+            lastSpawnTime = Time.time;
         }
-        if(Sw_3.intensity <= 0.7f)
+        if(Sw_3.intensity <= 0.7f && Time.time - lastSpawnTime >= spawnInterval)
         {
-            GameObject randomFlower_3 = flowerArray[Random.Range(0, flowerArray.Length)]; // 수정: 꽃 배열에서 랜덤한 값을 선택하도록 수정
+            GameObject randomFlower_3 = array_3[Random.Range(0, array_3.Length)]; 
             Instantiate(randomFlower_3, transform.position, Quaternion.identity);
+            lastSpawnTime = Time.time;
         }
     }
 }
