ea081cd08852e32d54eaf48196726bd6b4d70a8d
Taeng <xodn0720@naver.com>
2024-02-18 11:22:00 +0900

    flower update

--- Assets/01.Scripts/Flower.cs
+++ Assets/01.Scripts/Flower.cs
@@ -12,7 +12,7 @@ public class Flower : MonoBehaviour
     public GameObject[] array_3;
     public GameObject[] flowerArray; // 꽃 게임오브젝트 배열 추가
     private float lastSpawnTime = 0f;
-    private float spawnInterval = 30f; // 꽃 생성 간격
+    private float spawnInterval = 5f; // 꽃 생성 간격
 
     // Start is called before the first frame update
     void Start()
@@ -30,22 +30,25 @@ public class Flower : MonoBehaviour
         UnityEngine.Rendering.Universal.Light2D Sw_3 = Switch_3.GetComponent<UnityEngine.Rendering.Universal.Light2D>();
 
         // 각 스위치의 강도가 0.7 이하이고, 마지막 생성 시간에서 일정 시간이 경과한 경우에만 꽃 생성
-        if(Sw_1.intensity <= 0.7f && Time.time - lastSpawnTime >= spawnInterval)
+        if(Sw_1.intensity >= 0.7f && Time.time - lastSpawnTime >= spawnInterval)
         {
-            GameObject randomFlower_1 = array_1[Random.Range(0, array_1.Length)]; 
-            Instantiate(randomFlower_1, transform.position, Quaternion.identity);
+            GameObject randomFlower_1 = flowerArray[Random.Range(0, flowerArray.Length)]; 
+            GameObject randomObject_1 = array_1[Random.Range(0, array_1.Length)]; 
+            Instantiate(randomFlower_1, randomObject_1.transform.position, Quaternion.identity);
             lastSpawnTime = Time.time;
         }
-        if(Sw_2.intensity <= 0.7f && Time.time - lastSpawnTime >= spawnInterval)
+        if(Sw_2.intensity >= 0.7f && Time.time - lastSpawnTime >= spawnInterval)
         {
-            GameObject randomFlower_2 = array_2[Random.Range(0, array_2.Length)]; 
-            Instantiate(randomFlower_2, transform.position, Quaternion.identity);
+            GameObject randomFlower_2 = flowerArray[Random.Range(0, flowerArray.Length)]; 
+            GameObject randomObject_2 = array_2[Random.Range(0, array_2.Length)]; 
+            Instantiate(randomFlower_2, randomObject_2.transform.position, Quaternion.identity);
             lastSpawnTime = Time.time;
         }
-        if(Sw_3.intensity <= 0.7f && Time.time - lastSpawnTime >= spawnInterval)
+        if(Sw_3.intensity >= 0.7f && Time.time - lastSpawnTime >= spawnInterval)
         {
-            GameObject randomFlower_3 = array_3[Random.Range(0, array_3.Length)]; 
-            Instantiate(randomFlower_3, transform.position, Quaternion.identity);
+            GameObject randomFlower_3 = flowerArray[Random.Range(0, flowerArray.Length)]; 
+            GameObject randomObject_3 = array_3[Random.Range(0, array_3.Length)]; 
+            Instantiate(randomFlower_3, randomObject_3.transform.position, Quaternion.identity);
             lastSpawnTime = Time.time;
         }
     }
